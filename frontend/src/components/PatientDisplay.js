49d8de14757d2cfb351037b75bcb8311ebf72e3f
shwetalsoni <sonishwetal704@gmail.com>
2021-03-11 17:07:09 +0530

    table scrollable

--- frontend/src/components/PatientDisplay.js
+++ frontend/src/components/PatientDisplay.js
@@ -38,31 +38,33 @@ class PatientDisplay extends React.Component{
                 </div> 
                 <div className="row">
                     <div className="col-10 mx-auto">
-                        <table className="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Email</th>
-                                    <th scope="col">Dose 1</th>
-                                    <th scope="col">Dose 2</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {Object.keys(this.state.data).map((email, i)=> {
-                                    return (
-                                        <tr key={i}>
-                                            <th scope="row"><Link to={"/doctor_edit/" + email}>{i+1}</Link></th>
-                                            <td><Link to={"/doctor_edit/" + email}>{this.state.data[email].name}</Link></td>
-                                            <td><Link to={"/doctor_edit/" + email}>{email}</Link></td>
-                                            <td>{this.state.data[email].v1 ? (<p className="vac-btn">Vaccinated</p>) : (<p className="novac-btn">Not Vaccinated</p>)}</td>
-                                            <td>{this.state.data[email].v2 ? (<p className="vac-btn">Vaccinated</p>) : (<p className="novac-btn">Not Vaccinated</p>)}</td>
-                                        </tr>
-                                    )
-                                })}
-                                
-                            </tbody>
-                        </table>
+                        <div className="table-responsive">
+                            <table className="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Dose 1</th>
+                                        <th scope="col">Dose 2</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {Object.keys(this.state.data).map((email, i)=> {
+                                        return (
+                                            <tr key={i}>
+                                                <th scope="row"><Link to={"/doctor_edit/" + email}>{i+1}</Link></th>
+                                                <td><Link to={"/doctor_edit/" + email}>{this.state.data[email].name}</Link></td>
+                                                <td><Link to={"/doctor_edit/" + email}>{email}</Link></td>
+                                                <td>{this.state.data[email].v1 ? (<p className="vac-btn">Vaccinated</p>) : (<p className="novac-btn">Not Vaccinated</p>)}</td>
+                                                <td>{this.state.data[email].v2 ? (<p className="vac-btn">Vaccinated</p>) : (<p className="novac-btn">Not Vaccinated</p>)}</td>
+                                            </tr>
+                                        )
+                                    })}
+                                    
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>          
